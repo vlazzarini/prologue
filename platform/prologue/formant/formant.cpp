3d52a8446b8642bbb065da1fae124b93757dfd30
vlazzarini <victor.lazzarini@mu.ie>
2020-12-14 15:19:52 +0000

    taming the tenor formant

--- platform/prologue/formant/formant.cpp
+++ platform/prologue/formant/formant.cpp
@@ -29,56 +29,56 @@ const float bassf[] = {600,400,250,400,350,600,
 
 
 const float bassb[] = {60,40, 60, 40, 40, 60,
-                        70, 80, 90, 80, 80, 70,
-                        110, 100, 100, 100, 100, 110,
-                         120, 120, 120, 120, 120, 120};
+                       70, 80, 90, 80, 80, 70,
+                       110, 100, 100, 100, 100, 110,
+                       120, 120, 120, 120, 120, 120};
     
 const float bassa[] =  {0.45,0.25,0.031,0.27,0.1,0.45,
-                            0.35,0.35,0.15 ,0.1,0.032,0.35,
-                            0.35,0.25,0.1,0.1,0.04,0.35};
+                        0.35,0.35,0.15,0.1,0.032,0.35,
+                        0.35,0.25,0.1,0.1,0.04,0.35};
 
 const float tenorf[] = {650,400,290,400,350,650,
-                            1080,1700,1870,800,600,1080,
-                            2650,2600,2800,2600,2700,2650,
-                            2900,3200,3250,2800,2900,2900};
+                        1080,1700,1870,800,600,1080,
+                        2650,2600,2800,2600,2700,2650,
+                        2900,3200,3250,2800,2900,2900};
 
 
 const float tenorb[] = {80, 70, 40, 70, 40, 80,
-                            90, 80, 90, 80, 60, 90,
-                            120, 100, 100, 100, 100, 120,
-                            130, 120, 120, 130, 120, 130};
+                        90, 80, 90, 80, 60, 90,
+                        120, 100, 100, 100, 100, 120,
+                        130, 120, 120, 130, 120, 130};
     
 const float tenora[] =  {0.5,0.2,0.18,0.32,0.1,0.5,
-                         0.45,0.25,0.12,0.25,0.2,0.45,
-                          0.4,0.2,0.1,0.25,0.2,0.4};
+                         0.45,0.25,0.12,0.1,0.032,0.45,
+                         0.4,0.2,0.1,0.1,0.04,0.4};
   
 
 const float altof[] = {800,400,350,450,325,800,
-                           1150,1600,1700,800,700,1150,
-                           2800,2700,2700,2830,2530,2800,
-                           3500,3300,3700,3500,3500,3500};
+                       1150,1600,1700,800,700,1150,
+                       2800,2700,2700,2830,2530,2800,
+                       3500,3300,3700,3500,3500,3500};
 
 
 const float altob[] = {80, 60, 50, 70, 50, 80,
-                        90, 80, 100, 80, 60, 90,
-                           120, 120, 120, 100, 170, 120,
-                          130, 150, 150, 130, 180, 130};
+                       90, 80, 100, 80, 60, 90,
+                       120, 120, 120, 100, 170, 120,
+                       130, 150, 150, 130, 180, 130};
     
 const float altoa[] =  {0.63,0.063,0.1,0.35,0.25,0.63,
-                            0.1,0.031,0.031,0.15,0.031,0.1,
-                            0.015,0.015,0.015,0.04,0.01,0.015};
+                        0.1,0.031,0.031,0.15,0.031,0.1,
+                        0.015,0.015,0.015,0.04,0.01,0.015};
   
 
 const float soprf[] = {800,350,270,450,325,800,
-                           1150,2000,2140,800,700,1150,
-                           2900,2800,2950,2830,2700,2900,
-                           3900,3600,3900,3800,3800,3900};
+                       1150,2000,2140,800,700,1150,
+                       2900,2800,2950,2830,2700,2900,
+                       3900,3600,3900,3800,3800,3900};
 
 
 const float soprb[] = {80, 60, 60, 40, 50, 80,
-                           90, 100, 90, 80, 60, 90,
-                           120, 120, 100, 100, 170, 120,
-                           130, 150, 120, 120, 180, 130};
+                       90, 100, 90, 80, 60, 90,
+                       120, 120, 100, 100, 170, 120,
+                       130, 150, 120, 120, 180, 130};
     
 const float sopra[] =  {0.5,0.1,0.25,0.28,0.15,0.5,
                         0.03,0.16,0.05,0.1,0.017,0.03,
@@ -87,11 +87,11 @@ const float sopra[] =  {0.5,0.1,0.25,0.28,0.15,0.5,
 
 
 const float *frs[] = {(const float *) bassf, (const float *) tenorf,
-                       (const float *) altof, (const float *) soprf}; 
+                      (const float *) altof, (const float *) soprf}; 
 const float *bws[] = {(const float *) bassb, (const float *) tenorb,
-                       (const float *) altob, (const float *) soprb}; 
+                      (const float *) altob, (const float *) soprb}; 
 const float *amp[] = {(const float *) bassa, (const float *) tenora,
-                       (const float *) altoa,  (const float *) sopra}; 
+                      (const float *) altoa,  (const float *) sopra}; 
 
 
 // Simple linear AR envelope
@@ -175,19 +175,19 @@ void OSC_CYCLE(const user_osc_param_t *const params, int32_t *yn,
   const int sp3[] = {74, 72, 70, 68};
   
   float *bandwidth = (float *) (fno < 4 ? bws[fno] :
-    (note < sp1[fno-4] ? bws[0] :
-     (note < sp2[fno-4] ? bws[1] :
-      (note < sp3[fno-4] ? bws[2] : bws[3]))));
+                                (note < sp1[fno-4] ? bws[0] :
+                                 (note < sp2[fno-4] ? bws[1] :
+                                  (note < sp3[fno-4] ? bws[2] : bws[3]))));
   
   float *frequency =  (float *) (fno < 4 ? frs[fno] :
-    (note < sp1[fno-4] ? frs[0] :
-     (note < sp2[fno-4] ? frs[1] :
-      (note < sp3[fno-4] ? frs[2] : frs[3]))));
+                                 (note < sp1[fno-4] ? frs[0] :
+                                  (note < sp2[fno-4] ? frs[1] :
+                                   (note < sp3[fno-4] ? frs[2] : frs[3]))));
 
   float *amplitudes = (float *)  (fno < 4 ? amp[fno] :
-    (note < sp1[fno-4] ? amp[0] :
-     (note < sp2[fno-4] ? amp[1] :
-      (note < sp3[fno-4] ? amp[2] : amp[3]))));  
+                                  (note < sp1[fno-4] ? amp[0] :
+                                   (note < sp2[fno-4] ? amp[1] :
+                                    (note < sp3[fno-4] ? amp[2] : amp[3]))));  
 
   while(form >= 5.f) form -= 5.f;
   while(form < 0)  form += 5.f;
@@ -213,9 +213,9 @@ void OSC_CYCLE(const user_osc_param_t *const params, int32_t *yn,
     e = 1.f + amnt * env.proc();
     mod = osc_cosf(phase);
     y[i] = f32_to_q31(.25f*(obj.formant(ndx[0],ff[0]*e,phase,sphase,mod) +
-                       obj.formant(ndx[1],ff[1]*e,phase,sphase,mod)*amps[0] +
-                       obj.formant(ndx[2],ff[2]*e,phase,sphase,mod)*amps[1] +
-                       obj.formant(ndx[3],ff[3]*e,phase,sphase,mod)*amps[2]));
+                            obj.formant(ndx[1],ff[1]*e,phase,sphase,mod)*amps[0] +
+                            obj.formant(ndx[2],ff[2]*e,phase,sphase,mod)*amps[1] +
+                            obj.formant(ndx[3],ff[3]*e,phase,sphase,mod)*amps[2]));
     phase += w0;
     phase -= (uint32_t) phase;
     sphase += ws;
